04833fd22ca23685140d9d1f062dd22ece13b94e
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-09-06 20:55:37 +0300

    Updates

---
+++ precache-manifest.10147f8c5f29db977da07e85cd157031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412f8125e76f70f3cec2ee82d74d3eb8",
+    "url": "/liga-a-test/index.html"
+  },
+  {
+    "revision": "d619bf73527b3c8848ea",
+    "url": "/liga-a-test/static/js/2.70b98a5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/liga-a-test/static/js/2.70b98a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c30c8d2769f7dbe43e",
+    "url": "/liga-a-test/static/js/main.31ec4f88.chunk.js"
+  },
+  {
+    "revision": "3febe298746d10d98c34",
+    "url": "/liga-a-test/static/js/runtime-main.7e89ed99.js"
+  }
+]);
